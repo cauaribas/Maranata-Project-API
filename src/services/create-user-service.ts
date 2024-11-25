ed533e5dbe5eddac2069274183c33c3805f7c3b0
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-11-24 22:55:10 -0300

    feat: hash user passwords before storing in the database

--- src/services/create-user-service.ts
+++ src/services/create-user-service.ts
@@ -1,17 +1,20 @@
 import { User } from "../models/user";
 import { UserRepository } from "../repositories/user-repository";
+import bcrypt from 'bcrypt';
 
 export class CreateUserService {
   constructor(private userRepository: UserRepository) {}
 
-  async execute(data: User): Promise<User> {
+  async execute({ password, ...data }: User): Promise<User> {
     const existingUser = await this.userRepository.findByEmail(data.email);
     
     if(existingUser) {
       throw new Error('User already exists');
     }
 
-    const user: User = await this.userRepository.create(data);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user: User = await this.userRepository.create({ ...data, password: hashedPassword });
 
     return user;
   }
