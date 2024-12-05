4598850249e19a07f6e9ed26e5f42510855378c6
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-12-04 22:57:17 -0300

    feat: add deletePatient route for handling patient deletion requests

--- src/http/controllers/patients/routes.ts
+++ src/http/controllers/patients/routes.ts
@@ -3,9 +3,11 @@ import { create } from "./register-patient";
 import { verifyJWT } from "../../../http/middlewares/verify-jwt";
 import { fetch } from "./fetch-patients";
 import { changeStatus } from "./change-status";
+import { deletePatient } from "./delete-patient";
 
 export async function PatientRoutes(app: FastifyInstance) {
   app.post("/patients/register", { onRequest: verifyJWT }, create);
   app.get("/patients", { onRequest: verifyJWT }, fetch);
-  app.patch("/patients/status/:id", { onRequest: verifyJWT }, changeStatus)
+  app.patch("/patients/status/:id", { onRequest: verifyJWT }, changeStatus);
+  app.delete("/patients/:id", { onRequest: verifyJWT }, deletePatient);
 }
